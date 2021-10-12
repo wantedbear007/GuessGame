d5a4536a6e518b677618cde646616fe4db0c091c
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2021-10-12 17:24:17 +0530

    Developing GameScreen layout

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,23 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
 
-export const GameScreen = props => {};
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min) + min);
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
 
-const styles = StyleSheet.create({})
+export const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+
+  return;
+};
+
+const styles = StyleSheet.create({});
